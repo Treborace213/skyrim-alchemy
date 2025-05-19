9642458eed131d7ef5b4b147a469800761d8dc2d
Robert Dorling <RobbieDorling03@gmail.com>
2025-05-19 17:28:08 +0100

    Refactored DataManger

--- src/classes/DataManager.tsx
+++ src/classes/DataManager.tsx
@@ -1,7 +1,15 @@
 import { Effect } from "@/types/effect";
 import { Ingredient } from "@/types/ingredient";
+import { IngredientEffect } from "@/types/ingredientEffect";
 import { getBasePath } from "@/utils/basePath";
 
+// Paths to the Json Files
+const effectJsonPath = `${getBasePath()}/data/effects.json`;
+const baseIngredientsPath = `${getBasePath()}/data/legendaryAndSpecialEdition.json`;
+const anniversaryIngredientsPath = `${getBasePath()}/data/anniversaryEdition.json`
+
+
+// Interfaces for mapping.
 interface JsonEffect {
     Effect: string;
     IsPos: boolean;
@@ -24,66 +32,61 @@ interface JsonIngredient {
     effects: JsonIngredientEffect[];
 }
 
+
+// Main Class
 export class DataManager {
     effects: Effect[] = [];
     baseIngredients: Ingredient[] = [];
     anniversaryIngredients: Ingredient[] = [];
     loaded: boolean = false;
 
-    private async loadEffects(): Promise<void> {
-        try {
-            const filePath = `${getBasePath()}/data/effects.json`;
-            const response = await fetch(filePath);
-            if (!response.ok) throw new Error(`Network response was not ok for path: ${filePath} (Status: ${response.status})`);
-            const json = await response.json();
-
-            this.effects = json.map((item: JsonEffect) => ({
-                name: item.Effect,
-                isPos: item.IsPos,
-                description: item.Description,
-                baseCost: item["Base Cost"],
-                baseMag: item["Base Mag"],
-                baseDur: item["Base Dur"],
-                costAt100: item.CostAt100
-            }));
-        } catch (error) {
-            console.error("Error loading effects:", error);
-        }
-    }
-
-    private async getIngredients(filePath: string): Promise<Ingredient[]> {
+    private async fetchAndMapData<T>(filePath: string, mapFn: (item: any) => T): Promise<T[]> {
         try {
             const response = await fetch(filePath);
-            if (!response.ok) throw new Error(`Network response was not ok for path: ${filePath} (Status: ${response.status})`);
+            if (!response.ok) {
+                throw new Error(`Network response was not ok for path: ${filePath} (Status: ${response.status})`);
+            }
             const json = await response.json();
-
-            return json.map((item: JsonIngredient) => ({
-                name: item.name,
-                effects: item.effects.map((effect: JsonIngredientEffect) => ({
-                    effect: this.effects.find((e: Effect) => e.name === effect.name),
-                    magnitudeMult: effect.magnitude,
-                    durationMult: effect.duration,
-                    valueMult: effect.value
-                }))
-            }));
+            return json.map(mapFn);
         } catch (error) {
-            console.error("Error loading ingredients:", error);
+            console.error(`Error loading data from ${filePath}:`, error);
             return [];
         }
     }
 
-    private async loadBaseIngredients() {
-        this.baseIngredients = await this.getIngredients(`${getBasePath()}/data/legendaryAndSpecialEdition.json`)
+    private async getEffects(filePath: string): Promise<Effect[]> {
+        const effects = await this.fetchAndMapData(filePath, (item: JsonEffect) => ({
+            name: item.Effect,
+            isPos: item.IsPos,
+            description: item.Description,
+            baseCost: item["Base Cost"],
+            baseMag: item["Base Mag"],
+            baseDur: item["Base Dur"],
+            costAt100: item.CostAt100
+        }));
+
+        return effects;
     }
 
-    private async loadAnniversaryIngredients() {
-        this.anniversaryIngredients = await this.getIngredients(`${getBasePath()}/data/anniversaryEdition.json`);
+    private async getIngredients(filePath: string): Promise<Ingredient[]> {
+        const ingredients = await this.fetchAndMapData(filePath, (item: JsonIngredient) => ({
+            name: item.name,
+            effects: item.effects.map((effect: JsonIngredientEffect) => ({
+                effect: this.effects.find((e: Effect) => e.name === effect.name),
+                magnitudeMult: effect.magnitude,
+                durationMult: effect.duration,
+                valueMult: effect.value
+            })) as [IngredientEffect, IngredientEffect, IngredientEffect, IngredientEffect]
+        }));
+
+        return ingredients;
     }
 
     async loadData() {
-        await this.loadEffects();
-        await this.loadBaseIngredients();
-        await this.loadAnniversaryIngredients();
+        this.effects = await this.getEffects(effectJsonPath);
+        this.baseIngredients = await this.getIngredients(baseIngredientsPath);
+        this.anniversaryIngredients = await this.getIngredients(anniversaryIngredientsPath);
+
         this.loaded = true;
     }
 }
