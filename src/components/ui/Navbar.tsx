79da9dbaf5adcd15a7b6efb32a9fc3b92047cac4
Robert Dorling <RobbieDorling03@gmail.com>
2025-05-23 21:40:48 +0100

    Adjusted navbar alignment.

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -4,10 +4,10 @@ const NavBar: React.FC = () => {
   return (
     <nav
       /* Do not change the height with tailwind. Use --navbar-height. */
-      className="bg-menu px-6 flex justify-between items-center sticky top-0 z-100 mb-4"
+      className="bg-menu px-3 flex justify-between items-center sticky top-0 z-100 mb-4 space-x-6"
       style={{ height: 'var(--navbar-height' }}
     >
-      <div className="text-xl font-bold">Skyrim Alchemy</div>
+      <div className="text-xl font-bold text-center">Skyrim Alchemy</div>
       <ul className="flex space-x-6">
         <li>
           <Link href="/" className="hover:text-fg-tint transition-colors">Home</Link>
